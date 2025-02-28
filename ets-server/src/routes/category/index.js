7d046db373423bdecbfd84846ddffdd0ae02e469
Angelica Gutierrez <angelicagu368@gmail.com>
2025-02-27 16:08:32 -0800

    change route to categories

--- ets-server/src/routes/category/index.js
+++ ets-server/src/routes/category/index.js
@@ -40,4 +40,22 @@ router.post('/:expenseId', async (req, res, next) => {
         
 });
 
+router.get('/list', async (req, res, next) =>{
+    try{
+        // fetch all categories
+        const category = await Category.find();
+  
+
+        // send retrieved Categories as response
+        res.send({
+            message:'Categories retrieved successfully',
+            data: category
+        });
+    } catch (err) {
+        //category is not created 
+        console.error(`Error while retrieving categories${err}`);
+        next(err);
+    }
+});
+
 module.exports = router;
